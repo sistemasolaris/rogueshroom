36f6408f0d39f79df3b3fb56da55976272e4e2bf
Solaris <ovvo.system@gmail.com>
2024-03-10 12:38:03 -0300

    feat: use main function to run game

--- rogueshroom.py
+++ rogueshroom.py
@@ -1,16 +1,20 @@
 import pygame as pg
 
-pg.init()
-screen = pg.display.set_mode((1280, 720))
-clock = pg.time.Clock()
-running = True
+def main():
+    pg.init()
+    screen = pg.display.set_mode((1280, 720))
+    clock = pg.time.Clock()
+    running = True
 
-while running:
-    # Poll for events
-    for event in pg.event.get():
-        if event == pg.QUIT:
-            running = False
-    
-    screen.fill("purple")
-    pg.display.flip()
-    clock.tick(60) 
+    while running:
+        # Poll for events
+        for event in pg.event.get():
+            if event == pg.QUIT:
+                running = False
+        
+        screen.fill("purple")
+        pg.display.flip()
+        clock.tick(60)
+
+if __name__ == "__main__":
+    main()
