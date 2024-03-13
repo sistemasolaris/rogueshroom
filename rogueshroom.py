3f5186b0d4553a1b3a863a73f0b10e4bfc334809
Solaris <ovvo.system@gmail.com>
2024-03-13 11:31:00 -0300

    feat: draw player sprite on screen

--- rogueshroom.py
+++ rogueshroom.py
@@ -1,11 +1,18 @@
 import pygame as pg
 import config as conf
+from src.characters.player import Player
 
 def main():
     pg.init()
     screen = pg.display.set_mode((conf.WIDTH, conf.HEIGHT))
     clock = pg.time.Clock()
     running = True
+
+    # Sprite groups
+    all_sprites = pg.sprite.Group()
+
+    # Sprites
+    player = Player((640, 360), all_sprites)
 
     while running:
         # Poll for events
@@ -14,6 +21,8 @@ def main():
                 running = False
         
         screen.fill("purple")
+        all_sprites.draw(screen)
+        all_sprites.update()
         pg.display.flip()
         clock.tick(conf.FPS)
 
