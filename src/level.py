bb0f87be8452801b7cd7da58dd2a9ada9d60c095
Solaris <ovvo.system@gmail.com>
2024-03-22 17:34:10 -0300

    feat: create all_sprite sprite group

--- src/level.py
+++ src/level.py
@@ -3,7 +3,16 @@ from config import *
 
 class Level():
     def __init__(self):
+
+        # Game screen
         self.display_surface = pg.display.get_surface()
 
+        # Sprite groups
+        self.all_sprites = pg.sprite.Group()
+
     def run(self, dt):
+        
+        # Draw on screen and update sprites
         self.display_surface.fill("purple")
+        self.all_sprites.draw(self.display_surface)
+        self.all_sprites.update()
