46e408686c75e7bf8cd393e6980a27ded8e870b7
Solaris <ovvo.system@gmail.com>
2024-03-22 17:59:24 -0300

    feat: react to key presses in player object

--- src/characters/player.py
+++ src/characters/player.py
@@ -1,9 +1,25 @@
 import os
 import pygame as pg
-import config as conf
+from config import *
 
 class Player(pg.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
-        self.image = pg.image.load(os.path.join(conf.GRAPHICS_DIR, "characters", "player-character-prototype.png")).convert_alpha()
+        self.image = pg.image.load(os.path.join(GRAPHICS_DIR, "characters", "player-character-prototype.png")).convert_alpha()
         self.rect = self.image.get_rect(center = pos)
+
+    def update(self, dt):
+        self.input()
+
+    def input(self):
+        keys = pg.key.get_pressed()
+
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            print("up")
+        elif keys[pg.K_DOWN] or keys[pg.K_s]:
+            print("down")
+
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            print("left")
+        elif keys[pg.K_RIGHT] or keys[pg.K_d]:
+            print("right")
--- src/level.py
+++ src/level.py
@@ -22,4 +22,4 @@ class Level():
         # Draw on screen and update sprites
         self.display_surface.fill("purple")
         self.all_sprites.draw(self.display_surface)
-        self.all_sprites.update()
+        self.all_sprites.update(dt)
