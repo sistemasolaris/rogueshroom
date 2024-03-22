85f8be7b9895ca2779a7ccbd9fa602551b1660f1
Solaris <ovvo.system@gmail.com>
2024-03-22 17:29:13 -0300

    refact: change variable names in config.py

--- config.py
+++ config.py
@@ -5,8 +5,5 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 GRAPHICS_DIR = os.path.join(ROOT_DIR, "resources", "graphics")
 
 # Proportions
-WIDTH, HEIGHT = 1280, 720
+SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
 TILE = 16
-
-# Game variables
-FPS = 60
