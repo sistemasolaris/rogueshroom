5372763bbd19f8d76afeadfa7a45157b33972daa
Solaris <ovvo.system@gmail.com>
2024-03-26 17:49:58 -0300

    feat: make get_images_at method in SpriteSheet

--- src/utils/spritesheet.py
+++ src/utils/spritesheet.py
@@ -48,3 +48,22 @@ class SpriteSheet():
             image.set_colorkey(colorkey, pg.RLEACCEL)
         
         return image
+    
+    def get_images_at(self, rectangles: list[pg.Rect], colorkey: pg.color = None):
+        """
+        Get multiple sprites from sprite sheet as a list
+
+        Parameters
+        ----------
+        rectangles: list of pygame.Rect
+            The locations of the sprites to be returned, in the format of (x, y, width, height)
+        colorkey: pygame.Color, optional
+            The color, in (R, G, B), to be made transparent in the sprites, defaults to None
+
+        Returns
+        -------
+        list of pygame.Surface
+            A list of the requested sprites
+        """
+
+        return [self.get_image_at(rect, colorkey) for rect in rectangles]
